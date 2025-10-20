4fe196bab0c02a90d96441a24bc36871a23e4677
Matt Carroll <matthew-carroll@users.noreply.github.com>
2025-10-20 12:28:00 -0700

    WIP: Working on the floating editor toolbar and the keyboard panels (paused to try to fix Super Editor rebuilds recreates IME connection)

--- super_editor/example_chat/lib/floating_chat_editor_demo/fake_chat_thread.dart
+++ super_editor/example_chat/lib/floating_chat_editor_demo/fake_chat_thread.dart
@@ -3,11 +3,14 @@ import 'package:flutter/material.dart';
 /// A simulated chat conversation thread, which is simulated as a bottom-aligned
 /// list of tiles.
 class FakeChatThread extends StatelessWidget {
-  const FakeChatThread({super.key});
+  const FakeChatThread({super.key, this.scrollPadding = EdgeInsets.zero});
+
+  final EdgeInsets scrollPadding;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
+      padding: scrollPadding,
       reverse: true,
       // ^ The list starts at the bottom and grows upward. This is how
       //   we should layout chat conversations where the most recent
